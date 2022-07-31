fc7c7b977ae84f8839df99aedc1836aee1836cb7
Vladimir <Vladimir@y-solutions.ru>
2022-07-31 19:12:28 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
 public class Main {
+    public static void minSalary(Employee[] employees) {
+        String name = "";
+        int minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (minSalary > employee.getSalary()) {
+                minSalary = employee.getSalary();
+                name = employee.getName();
+            }
+        }
+        System.out.println("Employee with minimal salary is: " + name + ". His/her salary is: " + minSalary);
+    }
+    public static void maxSalary(Employee[] employees) {
+        String name = "";
+        int maxSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (maxSalary < employee.getSalary()) {
+                maxSalary = employee.getSalary();
+                name = employee.getName();
+            }
+        }
+        System.out.println("Employee with maximum salary is: " + name + ". His/her salary is: " + maxSalary);
+    }
     public static void main(String[] args) {
-        Employee[] workers = {
+        Employee[] workers = new Employee[]{
                 new Employee("John H.", 1, 1000),
-                new Employee("Copran L.", 2, 980),
-                new Employee("Copran L.", 2, 980),
+                new Employee("Mark L.", 2, 1080),
+                new Employee("Max K.", 3, 980),
+                new Employee("Marry I.", 4, 1000),
+                new Employee("John F.", 5, 1100),
+                new Employee("Mag G.", 1, 1050),
+                new Employee("Leon W.", 2, 960),
+                new Employee("Frost M.", 3, 970),
+                new Employee("Brock N.", 4, 900),
+                new Employee("Nita B.", 5, 990),
         };
-        PrintService printService = new PrintService();
+        printService printService = new printService();
         printService.print(workers);
-
+        minSalary(workers);
+        maxSalary(workers);
     }
 }
--- src/printService.java
+++ src/printService.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class printService {
+public class printService {
+    public void print(Employee[] employees) {
+        System.out.println("Size of workers list: " + employees.length);
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            System.out.println("Name: " + employee.getName() + "; department: " + employee.getDepartment() + "; salary: " + employee.getSalary() + ".");
+        }
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            sum = sum + employee.getSalary();
+        }
+        System.out.println("Sum of all salaries is: " + sum);
+        int av = sum / employees.length;
+        System.out.println("Average Salary is: " + av);
+    }
 }
